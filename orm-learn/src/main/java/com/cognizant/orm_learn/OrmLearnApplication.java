c66f2a3be19a695ff81b023cf685c758df2d9d6c
PantangiCharitha <charithapantangi07@gmail.com>
2026-07-20 11:25:37 +0530

    Completed Spring Data JPA Hands-on 1, 2 and 3

---
+++ orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,74 @@
+package com.cognizant.orm_learn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+
+    ApplicationContext context =
+            SpringApplication.run(OrmLearnApplication.class, args);
+
+    LOGGER.info("Inside main");
+
+    countryService = context.getBean(CountryService.class);
+
+   
+
+    testAddCountry();
+}
+    private static void testGetAllCountries() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries =
+                countryService.getAllCountries();
+
+        LOGGER.debug("Countries = {}", countries);
+
+        LOGGER.info("End");
+    }
+	private static void testGetCountry() {
+
+    LOGGER.info("Start");
+
+    Country country = countryService.getCountry("IN");
+
+    LOGGER.debug("Country = {}", country);
+
+    LOGGER.info("End");
+
+}
+private static void testAddCountry() {
+
+    LOGGER.info("Start");
+
+    Country country = new Country();
+
+    country.setCode("JP");
+    country.setName("Japan");
+
+    countryService.addCountry(country);
+
+    Country newCountry = countryService.getCountry("JP");
+
+    LOGGER.debug("Added Country = {}", newCountry);
+
+    LOGGER.info("End");
+}
+}
